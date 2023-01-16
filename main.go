09298d07c0c96cd46d48ea39c6aaaadffc21f0c1
Himanshu <himanshumishra500@gmail.com>
2023-01-16 22:15:43 +0530

    db working

--- main.go
+++ main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	"MYPLANTBACKEND/service"
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type todo struct {
-	ID        string `json:"id"`
-	Item      string `json:"title"`
-	Completed bool   `json:"completed"`
+type user struct {
+	Email           string `json:"email"`
+	DeviceId        string `json:"deviceId"`
+	DeviceType      string `json:"deviceType"`
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	UpdatedAt       string `json:"updatedAt"`
+	ProfilePhotoUrl string `json:"profilePhotoUrl"`
+	Registered      bool   `json:"registered"`
+	MobileNumber    string `json:"mobileNumber"`
 }
 
-var todos = []todo{
-	{ID: "1", Item: "Clean Room", Completed: false},
-	{ID: "2", Item: "Read Book", Completed: true},
-}
+// var todos = []todo{
+// 	{ID: "1", Item: "Clean Room", Completed: false},
+// 	{ID: "2", Item: "Read Book", Completed: true},
+// }
 
-func getTodos(context *gin.Context) {
+func getTodos(context *gin.Context, DB *sql.DB) {
+	var res user
+	var todos []user
+	rows, err := DB.Query("SELECT * FROM public.user")
+	if err != nil {
+		fmt.Println(err)
+		context.IndentedJSON(http.StatusBadGateway, "An error occured")
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
@@ -27,7 +48,12 @@ func main() {
 	MqttCon := &service.MQTTConnector{Client: nil, SubCh: "register-service"}
 	MqttCon.Start()
 
+	DbCon := &service.DBConnector{DB: nil}
+	DbCon.Start()
+
 	router := gin.Default()
-	router.GET("/todos", getTodos)
+	router.GET("/todos", func(context *gin.Context) {
+		getTodos(context, DbCon.DB)
+	})
 	router.Run("localhost:8080")
 }
---
+++ service/database.go
@@ -0,0 +1,90 @@
+package service
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type DBConnector struct {
+	// db gorm.DB
+	DB *sql.DB
+}
+
+var connectionName string
+var dbuser string
+var dbpassword string
+var dbname string
+var dbport string
+
+func (c *DBConnector) Start() {
+	connectionName = os.Getenv("INSTANCE_CONNECTION_NAME")
+	dbuser = os.Getenv("DB_USER")
+	dbpassword = os.Getenv("DB_PASS")
+	dbname = os.Getenv("DB_NAME")
+	dbport = os.Getenv("DB_PORT")
+
+	// connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", connectionName, dbport, dbuser, dbpassword, dbname)
+	connString := "postgresql://postgres:quicuxeo@localhost/core-service?sslmode=disable"
+	fmt.Print(connString)
+
+	var err error
+
+	c.DB, err = sql.Open("postgres", connString)
+
+	// c.db, err = gorm.Open(postgres.New(postgres.Config{
+	// 	DriverName: "cloudsqlpostgres",
+	// 	DSN:        connString,
+	// }))
+
+	// Connect to the Postgres database on Google Cloud SQL
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+// func (c *DBConnector) GetUser() {
+// 	var users []User
+// 	db.Find(&users)
+
+// 	return users
+// 	// c.JSON(http.StatusOK, gin.H{"data": users})
+// }
+
+// func main() {
+
+// 	// Use Gin to handle HTTP requests
+// 	r := gin.Default()
+// 	r.GET("/data", func(cgin.Context) {
+// 		var users []User
+// 		db.Find(&users)
+// 		c.JSON(http.StatusOK, gin.H{"data": users})
+// 	})
+
+// 	r.GET("/", func(c gin.Context) {
+// 		c.JSON(http.StatusOK, gin.H{
+// 			"message": "Hello, World!",
+// 		})
+// 	})
+
+// 	r.POST("/saveUser", func(cgin.Context) {
+// 		us := User{ID: 1, Username: "mukh"}
+// 		log(us.ID)
+// 		logs(us.Username)
+// 		if !db.Migrator().HasTable("users") {
+// 			db.Migrator().CreateTable(&User{})
+// 		}
+// 		res := db.Create(&us)
+// 		logs(res.Error.Error())
+// 	})
+// 	r.Run(":8080")
+// }
